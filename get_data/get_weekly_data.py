d28399fbfb6d1ad7b67f612abc3448084f57bec3
Sllcc <72741615+MOquan123@users.noreply.github.com>
2024-04-09 11:17:04 +0800

    Add files via upload

---
+++ get_data/get_weekly_data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+import baostock as bs
+from datetime import datetime, timedelta
+
+def get_weekly_data(code_name_dict, period=60):
+    # Determine the start and end date based on the period
+    start_date = (datetime.today() - timedelta(weeks=period)).strftime('%Y-%m-%d')
+    end_date = datetime.today().strftime('%Y-%m-%d')
+        
+    data_folder = 'output/w_data'
+
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+        print(f'Folder {data_folder} created.')
+    else:
+        print(f'Folder {data_folder} already exists.')
+
+    # Fetch and save the historical data for each stock
+    for code, name in code_name_dict.items():
+        print(f"Fetching data for {code}:{name}")
+
+        # Query for the historical k-line basic data
+        rs = bs.query_history_k_data_plus(
+            code,
+            "date,code,open,high,low,close,volume,amount",
+            start_date=start_date,
+            end_date=end_date,
+            frequency="w",  # use "w" for weekly data
+            adjustflag="3"  # "3" for no adjust
+        )
+        
+        if rs.error_code != '0':
+            print(f'Failed to query history k data, error_msg: {rs.error_msg}')
+       
+        data_list = []
+        while (rs.error_code == '0') & rs.next():
+            data_list.append(rs.get_row_data())
+        df = pd.DataFrame(data_list, columns=rs.fields)
+    
+        print(f'Data for {code}:{name}:')
+        print(df.head())
+        if df.empty:
+            continue
+        df.to_csv(f'{data_folder}/{code}_{name}.csv')
